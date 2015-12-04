dbb14cbe03fc0ce008895f6fb0bed64e8637c086
AndyTarilton <atarilton@ups.edu>
2015-12-03 20:45:22 -0800

    Create addCharacter.php

---
+++ addCharacter.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+Create a new character!
+</head>
+
+<body>
+
+        <form action="executeAddCharacter.php" method="post">
+                <div> CharacterName: <input type = "text" name = "Character Name" required="required"/>
+                
+
+                <input type = "submit">
+        </form>
+
+</body>
+
+</html>
+
