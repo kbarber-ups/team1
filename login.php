437e90bcf4682621e0d309edaa1bd212e60b4c98
AndyTarilton <atarilton@ups.edu>
2015-12-05 15:04:52 -0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>User Logon</title>
+</head>
+<body>
+  <h2>User Login </h2>
+  <form name="login" method="post" action="executeLogin.php">
+   Username: <input type="text" name="tryUsername"><br>
+   Password: <input type="password" name="tryPassword"><br>
+   <input type="submit" name="submit" value="Login!">
+  </form>
+</body>
+</html>
